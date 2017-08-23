0b77d37e4d79f7020bb556a150f642026043a2c0
mfergu <mfergu3@g.clemson.edu>
2017-08-23 09:54:20 -0400

    added music and segues

--- TankGame.py
+++ TankGame.py
@@ -1,10 +1,24 @@
 #tank game
 #color chart https://www.w3schools.com/colors/colors_picker.asp
 #music MASTER BOOT RECORD https://www.youtube.com/watch?v=s1522TU8qeE
-#sprites https://www.pygame.org/docs/tut/SpriteIntro.html
+# pygame music https://www.pygame.org/docs/ref/music.html#comment_pygame_mixer_music_load
+#sprites http://programarcadegames.com/index.php?chapter=introduction_to_sprites
+#sprite libraries https://opengameart.org/content/starter-kits
 #similar https://codereview.stackexchange.com/questions/117875/space-shooter-made-using-pygame
 
 import pygame, sys, random, time
+from os import path
+
+######################################################
+##asset dirs
+img_dir = path.join(path.dirname(__file__),'sprites/PNG')
+sound_dir = path.join(path.dirname(__file__),'music')
+
+######################################################
+WIDTH = 800
+HEIGHT = 800
+FPS = 60
+POWERUP_TIME = 5000
 
 #colors
 colors = {'red' : pygame.Color(255, 0, 0),
@@ -14,14 +28,96 @@ colors = {'red' : pygame.Color(255, 0, 0),
         'white' : pygame.Color(255, 255, 255),
         'brown' : pygame.Color(165, 42, 42),
         'tan' : pygame.Color(255, 204, 102)}
+######################################################
+
+######################################################
+##initialize pygame and create window
+
+check_errors = pygame.init()
+if check_errors[1] > 0:
+    print("(!) Had {0} initializing errors,\
+    exiting...".format(check_errors[1]))
+    sys.exit("-1")
+else:
+    print("(+) PyGame successfully initialized!")
+# setup music
+pygame.mixer.init()
+#play surface
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption('Tanks')
+#frame controller
+#for syncing the fps
+fpsController = pygame.time.Clock() 
+#game font
+font_name = pygame.font.match_font('arial')
+######################################################
+     
+
+def main_menu():
+    global screen
+    menu_song = pygame.mixer.music.load(path.join(sound_dir, "MBR-DOOM.ogg"))
+    pygame.mixer.music.play(-1)
+
+    title = pygame.image.load(path.join(img_dir, "main.jpg")).convert()
+    title = pygame.transform.scale(title, (WIDTH, HEIGHT), screen)
+    screen.blit(title, (0,0))
+    pygame.display.update()
+
+    while True:
+        ev = pygame.event.poll()
+        if ev.type == pygame.KEYDOWN:
+            if ev.key == pygame.K_RETURN:
+                break
+            elif ev.key == pygame.K_q or ev.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        else:
+            draw_text(screen, "Press [ENTER] To Begin", 30, WIDTH/2, HEIGHT/2)
+            draw_text(screen, "or [Q] to Quit", 30, WIDTH/2, (HEIGHT/2)+40)
+            pygame.display.update()
+
+    ready = pygame.mixer.Sound(path.join(sound_dir,'segue.ogg'))
+    ready.play()
+    screen.fill(colors['black'])
+    draw_text(screen, "GET READY!", 40, WIDTH/2, HEIGHT/2)
+    pygame.display.update()
+    pygame.time.wait(2)
+    pygame.mixer.music.stop()
+
+
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, colors['white'])
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
+
+def draw_shield_bar(surf, x, y, pct):
+    if pct < 0:
+        pct = 0
+    BAR_LENGTH = 100
+    BAR_HEIGHT = 10
+    fill = (pct / 100) * BAR_LENGTH
+    outline_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+    pygame.draw.rect(surf, colors['green'], fill_rect)
+    pygame.draw.rect(surf, colors['white'], outline_rect, 2)
+
+def draw_lives(surf, x, y, lives, img):
+    for i in range(lives):
+        img_rect= img.get_rect()
+        img_rect.x = x + 30 * i
+        img_rect.y = y
+        surf.blit(img, img_rect)
+
 
 
 #vehic class
-class vehic(object):
+class vehic(pygame.sprite.Sprite):
     #attributes name, hp, fuel, armor, ammo, 
     #acceleration
 
     def __init__(self, name, hp, fuel, armor, ammo, posx, posy, max_speed, direction):
+        super.init()
         self.name = name
         self.hp = hp
         self.armor = armor
@@ -33,10 +129,11 @@ class vehic(object):
 
 
 #base class for attacks
-class shot(object):
+class shot(pygame.sprite.Sprite):
     #attributes x, y, distance, direction, acceleration
 
     def __init__(self, posx, posy, direction, distance, speed):
+        super.init()
         self.posx = posx
         self.posy = posy
         self.direction = direction
@@ -44,9 +141,10 @@ class shot(object):
         self.speed = speed
 
 #inherits from shot
-class fan(shot):
+class fan(pygame.sprite.Sprite):
     #fan in deg
     def __init__(self, posx, posy, direction, distance, speed, fan):
+        super.init()
         self.posx = posx
         self.posy = posy
         self.direction = direction
@@ -54,57 +152,41 @@ class fan(shot):
         self.speed = speed
         self.fan = fan
 
-class package(object):
+class care_package(pygame.sprite.Sprite):
     #attributes support type, amount
 
     def __init__(self, sup_type, amount, posx, posy):
+        super.init()
         self.sup_type = sup_type
         self.amount = amount
         self.posx = posx
         self.posy = posy
 
 # Game over function
-def gameOver():
+def game_over():
     myFont = pygame.font.SysFont('monaco', 72)
-    GOsurf = myFont.render('Game Over!', True, red)
+    GOsurf = myFont.render('Game Over!', True, colors['red'])
     GOrect = GOsurf.get_rect()
     GOrect.midtop = (360, 20)
-    playSurface.blit(GOsurf,GOrect) 
+    screen.blit(GOsurf,GOrect) 
     pygame.display.flip()
     time.sleep(4)
     pygame.quit() #pygame exit
     sys.exit() #console exit
 
        
+main_menu()
+game_over()
 
 #import variables
 
 
 #direction = 'RIGHT'
 #changeto = direction
+"""
 
-   #main game logic
-check_errors = pygame.init()
-
-if check_errors[1] > 0:
-    print("(!) Had {0} initializing errors,\
-    exiting...".format(check_errors[1]))
-    sys.exit("-1")
-else:
-    print("(+) PyGame successfully initialized!")
-    # setup music
-    pygame.mixer.init()
-
-    #play surface
-    playSurface = pygame.display.set_mode((800,800))
-    pygame.display.set_caption('Tanks')
-    foodPos = [random.randrange(1,72)*10 ,random.randrange(1,46)*10]
-    foodSpawn = True
-     
-    #frame controller
-    fpsController = pygame.time.Clock()
-
-
+foodPos = [random.randrange(1,80)*10 ,random.randrange(1,80)*10]
+foodSpawn = True
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -169,3 +251,4 @@ else:
                 gameOver()
         pygame.display.flip()
         fpsController.tick(15)
+        """
